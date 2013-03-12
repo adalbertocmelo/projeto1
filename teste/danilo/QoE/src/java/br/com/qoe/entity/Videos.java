60cec0410b24f032ee97a8e31f5d726bac3a70d3
Danilo Souza <dhcsouza@gmail.com>
2013-03-12 10:39:54 -0300

    Adição da tag de "id" na classe de videos

--- teste/danilo/QoE/src/java/br/com/qoe/entity/Videos.java
+++ teste/danilo/QoE/src/java/br/com/qoe/entity/Videos.java
@@ -17,6 +17,7 @@ import javax.persistence.*;
 public class Videos implements Serializable{
     
     @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
     private int id;
     
     private String nome;
